ee7351ae5f04c316b1a0bedc8bfbd3145cfcb5dc
HabitamuAsmare <habitamugetaneh2018@gmail.com>
2024-07-16 02:46:19 -0500

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
+import Row from '../../Components/Rows/Row/Row'
+import RowList from '../../Components/Rows/RowList/RowList'
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner/>
+      <Row/>
+      <RowList></RowList>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home;
---
+++ netflix-clone/src/utils/requests.js
@@ -0,0 +1,20 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-Us&page=1`
+  // Additional Rows Start
+  //   fetchwestern: `/discover/movie?api_key=$(API_KEY}&with_genres=37`,
+  //   fetchwar: `/discover/movie?api_key=${API_KEY}&with_geners=10752`,
+  //   fetchCrime: `/discover/movie?api_key=${API_KEY}&with_genres=80`,
+  // Additional Rows End
+};
+export default requests;
